9f4abf9813c482166bd01f4c7ab20c586cc94e6c
Pranav Kulkarni <pranavcode@gmail.com>
2015-11-05 03:59:31 +0530

    Add game engine files

---
+++ game_engine/team1.c
@@ -0,0 +1,7 @@
+#include "stdio.h"
+
+
+int team1(puck boardCheckers[8][8],int player,char array[32][4],int k)
+{
+               return 0;
+}
---
+++ game_engine/team2.c
@@ -0,0 +1,6 @@
+#include "stdio.h"
+
+int team2(puck boardCheckers[8][8],int player,char array[32][4],int k)
+{
+               return 0;
+}
